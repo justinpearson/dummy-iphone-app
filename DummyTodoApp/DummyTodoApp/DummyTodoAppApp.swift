14feffef41f74e4a7c883914d95896b3ca8beb5a
Justin Pearson <justinpearson@users.noreply.github.com>
2026-01-07 06:23:49 -0800

    empty iOS project created by xcode

---
+++ DummyTodoApp/DummyTodoApp/DummyTodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DummyTodoAppApp.swift
+//  DummyTodoApp
+//
+//  Created by Justin Pearson on 1/7/26.
+//
+
+import SwiftUI
+
+@main
+struct DummyTodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DummyTodoApp/DummyTodoAppTests/DummyTodoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DummyTodoAppTests.swift
+//  DummyTodoAppTests
+//
+//  Created by Justin Pearson on 1/7/26.
+//
+
+import Testing
+@testable import DummyTodoApp
+
+struct DummyTodoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
